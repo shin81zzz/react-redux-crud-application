90d980dfc9daf2b642fb0011912cb9ebcc6c080c
shin81zzz <ss.fukim@gmail.com>
2019-12-09 08:24:17 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
+// const App = () => (<Counter></Counter>)
 function App() {
-  const profiles = [
-    {
-      name: "Taro", age: 10
-    },
-    {
-      name: "Hanako", age: 5
-    },
-    {
-      name: "NoName"
-    }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return < User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  );
+  return (<Counter></Counter>)
 }
 
-function User(props) {
-  return <div>Hi! I am {props.name}, and {props.age} years old!</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
 
-User.PropTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>count: {this.state.count}</h1>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
