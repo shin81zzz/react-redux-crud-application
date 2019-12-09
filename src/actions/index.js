e04b35a92fe0f3bcfb214bd5a70c04d88b227356
shin81zzz <ss.fukim@gmail.com>
2019-12-09 20:56:13 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// export const increment = () => {
+//     return {
+//         type: 'INCRIMENT'
+//     } 
+// }
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+// export const decrement = () => {
+//     return {
+//         type: 'DECRIMENT'
+//     }
+// }
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
